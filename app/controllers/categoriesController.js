592220873425739bb37335b5ed9374b2649f837e
Niranjan Kumar <niranjan32@gmail.com>
2022-02-21 10:39:07 +0530

    updated alignment

--- app/controllers/categoriesController.js
+++ app/controllers/categoriesController.js
@@ -8,18 +8,18 @@ const Category = require('../models/category')
         .catch((err) => {
             res.json(err)
         })  
-  }
+   }
 
  module.exports.create = (req,res)=>{
-    const body = req.body
-    const category= new Category(body)
-    category.save()
-    .then((category)=>{
-        res.json(category)
-    })
-    .catch((err)=>{
-        res.json(err)
-    })
+        const body = req.body
+        const category= new Category(body)
+        category.save()
+        .then((category)=>{
+            res.json(category)
+        })
+        .catch((err)=>{
+            res.json(err)
+        })
  }
 
  module.exports.show= (req,res)=>{
